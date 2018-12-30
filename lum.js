997436ebdfdb79affdebebb7f1fc7b17bb780128
integralyogin <integralyogin@gmail.com>
2018-12-30 05:54:10 -0500

    made new simplified Parsing method
    makes program more intuitive, automatically searches keys first unless a database is specified.

--- lum.js
+++ lum.js
@@ -1,26 +1,19 @@
 function check_address(filename, req) { 
-    address = window.location.pathname
-    address = String(address)
-
-    if (address == "/home/oem/Documents/Code/lum.html") {
-        console.log("LOCAL")
+    address = String(window.location.pathname)
+    if (address == "/home/oem/Documents/Code/lum.html") { // LOCAL
         return filename
-    } 
-    else {
-        console.log("ONLINE")
+    } else { // ONLINE
         newfilename = "https://raw.githubusercontent.com/integralyogin/data-for-lum/master/"+filename
         return newfilename
     }
 }
 
+
 function setUp(event, filename) {
     var req = new XMLHttpRequest();
     newfilename = check_address(filename, req)
-    console.log("newFILENAME:")
-    console.log(newfilename)
     req.open("GET", newfilename, false);
     req.send(null);
-    var values;
     return req
 }
 
@@ -28,45 +21,63 @@ function setUp(event, filename) {
 function process(text) {
     for (var i = 0; i < text.length; i++) {
         new_text = text[i].split(" ")
-        //console.log(i, new_text[1])
-        if (new_text[1] == "1") {
-            console.log(new_text)
-            console.log(typeof new_text)
-            return new_text
-        }
+        if (new_text[1] == "1") { return new_text; }
     }
 }
 
 
-function new_getInput(event) {
+function Parser(userInput) {  
+    if ((userInput.split(" ").length) > 1) { return userInput.split(" "); }
+    if ((userInput.split(" ").length) < 2) { return userInput; }
+} 
+
+
+function newParser(userInput, event) { 
+    list_of_dbs = ["keys", "arex", "QQ", "gloss", "san", "iye", "aqal", "occ","auth", "cats", "syn"]
+    userInput = Parser(userInput)
+    db = "keys"
+    for (x = 0; x < list_of_dbs.length; x++) { // CLEAN DB_ARG IF ONE EXISTS
+        if (userInput[0] == list_of_dbs[x]) { 
+            db = list_of_dbs[x]; 
+            userInput = userInput.splice(1);
+            console.log("FOUND DB")
+        } 
+    }
+    new_arg = userInput
+    userInput = String(userInput)
+    if (userInput.split(",").length > 1) {
+        for (var x = 0; x < Number(userInput.split(",").length)-1; x++) { // PREPRINT SET UP
+            if (x < userInput.split(",").length-1) { new_arg += " " }
+        }
+    }
+    userInput = userInput.replace(/,/g, " ")
+    prePrint(event, db, userInput)
+    return userInput
+}
+
+
+function prePrint(event, db, userInput) {
+    req = setUp(event, db)
+    regexPrint(req, userInput)
+}
+
+function getInput(event) {
     userInput = document.getElementById("myinput").value;
-    // req = setUp(event, userInput, keys)
-    if (event.keyCode == 13) { 
-        console.log("enter")
-        arguments = Parser(userInput)
-        if (userInput.split(" ").length > 1) { //if 2+ args
-            req = setUp(event, arguments[0].toLowerCase())
-            new_arg = ""
-            for (var x = 1; x < arguments.length; x++) { // prep 2nd part of arg
-                new_arg += arguments[x]
-                if (x < arguments.length-1) { new_arg += " " }
-            console.log(new_arg)
-            }
-            regexPrint(req, new_arg)
-        }
-        if (userInput.split(" ").length == 1) { // if no arg
-            req = setUp(event, "keys")
-            regexPrint(req, userInput); 
-        }
+    document.getElementById("myinput").focus();
+
+    if (event.keyCode == 13) { // ENTER KEYPRESS
+        arguments = newParser(userInput, event)
     }
-    if (event.keyCode == 9) { // left tab
+    if (event.keyCode == 9) {       // LEFT TAB KEYPRESS
         document.getElementById("myinput").scrollIntoView();
+        document.getElementById("myinput").focus(); 
     }
-    if (event.keyCode == 16) { //left shift
-        console.log("left shift")
+    if (event.keyCode == 16) {      // LEFT SHIFT KEYPRESS
         removeChildren()
     }
+
 }
+
 
 function removeChildren() {
     for (x = 0; x < document.getElementById("output").childNodes.length; x++) { 
@@ -81,49 +92,32 @@ function removeChildren() {
 function regexPrint(file, input) { 
     var count = 0
     var query = new RegExp(".*"+input+".*", "gim");
-    var para = document.createElement("P"); 
-    var out = document.getElementById("output");
-
 	values = file.responseText.match(query);
 
     for (var x = 0; x < values.length; x++) {
-        out.appendChild(document.createTextNode(values[x]));
+        document.getElementById("output").appendChild(document.createTextNode(values[x]));
         if (values[x] != "") { 
-            out.appendChild(document.createElement("br"));
-            out.appendChild(document.createElement("br"));
+            document.getElementById("output").appendChild(document.createElement("br"));
+            document.getElementById("output").appendChild(document.createElement("br"));
         }
         count++;
     }
-    out.appendChild(document.createTextNode(count));    
-    //out.appendChild(document.createTextNode("<a href=\"#top\">top</a>"))
+    document.getElementById("output").appendChild(document.createTextNode(count));
 }
 
 
 
-function Parser(userInput) {  
-    if ((userInput.split(" ").length) > 1 ) { // 2+ args
-        var argument = userInput.split(" ");
-        return argument
-    }
-    if ((userInput.split(" ").length) < 2) { // 1 arg
-        console.log("userInput.split.length:::")
-        console.log(userInput.split(" ").length)
-        return userInput
-    }
-} 
-
-
-  
-window.addEventListener('keydown', function (event) { //removes tabs previous functionality
+window.addEventListener('keydown', function (event) { // disables some keys
     if (event.keyCode === 9) { //left tab
         event.preventDefault();
         return false;
     }
-    if (event.keyCode === 16) { //left shift?
+    if (event.keyCode === 16) { //left shift
         event.preventDefault();
         return false;
     }
 });
 
-    //document.getElementById("myinput").addEventListener("keyup", new_getInput); 
-  addEventListener("keyup", new_getInput);
+    //document.getElementById("myinput").addEventListener("keyup", getInput); 
+  addEventListener("keyup", getInput);
+
