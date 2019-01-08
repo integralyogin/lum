9578e16f0ca02e104f1cd55e0f39b1ae524a1756
integralyogin <integralyogin@gmail.com>
2019-01-07 20:13:52 -0500

    uhh

--- lum.js
+++ lum.js
@@ -1,13 +1,82 @@
-
 <script>
+
+
+function mouseOn(x) {
+    // check wikiauthors for author name
+    wikit = setUp("wikiauthors")
+    //input = "Sri Aurobindo"
+    var node = document.getElementById('demo')
+    var node2 = document.getElementById('output')
+    //node2 = node.getChild(getElementById('demo'+x))
+    console.log("node: "+node)
+    console.log("node2: "+node2)
+    textContent1 = node.childNodes[0].innerHTML
+    textContent2 = node2.childNodes[0].innerHTML
+    //var resBox=document.getElementById('demo'+x);
+    //var strTrans=resBox.getElementsByTagName('span').textContent
+    //console.log("strTrans: "+strTrans)
+    //
+    console.log("textContent1: "+textContent1)
+    console.log("textContent2: "+textContent2)
+    text_length = textContent1.length
+    authName = textContent1.slice(1, text_length-4)
+    console.log("authName ::: "+authName)
+    authName = ""
+
+    //console.log("documents.getelement "+document.getElementById('output').getChild(document.getElementById('demo')))
+    //ele = document.getElementById('output')
+    //ele = ele.getElementById('demo').textContent
+    //ele = document.getElementById('demo'+x).textContent
+   // console.log("ele: "+ele)
+    
+
+
+    //document.getElementById("demo2").appendChild(document.createTextNode(authName));
+    var query = new RegExp("^"+authName+".*", "img");
+    entry = (wikit.responseText.match(query));
+    //savitri_list.push(values[x]);
+
+
+    for (var i = 0; i < entry.length; i++) {
+        if (authName != "") {
+            if (entry[i].includes(authName)) {
+                console.log("authName :"+authName)
+                console.log("match "+entry[i])
+                x = i
+            }
+        }
+    }
+
+    document.getElementById("demo"+x).appendChild(document.createTextNode("values"+x+": "+entry[x]));
+    //console.log("mouseOn: entry[x]: "+entry[x])
+    //console.log(entry)
+    
+}
+
+function mouseOff(x) { 
+    new_removeChildren("demo"+x) 
+    //var x = document.getElementById("demo");    
+    //x.style.color = "black";
+    //var z = document.getElementById("demo2");  
+    //document.getElementById("demo2").appendChild(document.createTextNode("+"));
+}
+
 function check_address(filename, req) { 
+    console.log("check_address: filename: ", filename)
     address = String(window.location.pathname)
     if (address == "/home/oem/Documents/Code/KEYS/lum.html") { console.log("LOCAL");
         return filename
-    } else { console.log("ONLINE");    
+    } else { 
+        console.log("ONLINE");    
         newfilename = "https://raw.githubusercontent.com/integralyogin/data-for-lum/master/"+filename
+        if (filename.includes('fulldatabase')) {
+            newfilename = "https://gitlab.com/integralyogin/fulldatabase/blob/master/"+filename
+            newfilename = "https://gitlab.com/integralyogin/fulldatabase/raw/0f997955e3a092d8062556eb0e4658e291ff43bf/"+filename
+        }
         return newfilename
     }
+
+// https://gitlab.com/integralyogin/fulldatabase/blob/master/fulldatabase
 }
 
 
@@ -15,7 +84,7 @@ function consoleInfo(text) {
     console.log("consoleInfo(): \""+text+"\"")
 }
 
-function setUp(event, filename) {
+function setUp(filename) {
     var req = new XMLHttpRequest();
     newfilename = check_address(filename, req)
     req.open("GET", newfilename, false);
@@ -30,13 +99,6 @@ function process(text) {
         if (new_text[1] == "1") { return new_text; }
     }
 }
-
-
-function auth_list(event) { 
-    setUp(event, "keys")
-}
-
-
 
 
 function getInput(event) {
@@ -59,7 +121,7 @@ function getInput(event) {
             brIndex = savitri_list[indexNum].indexOf("<br>")
             stem = savitri_list[indexNum].slice(1,brIndex)
             console.log("getInput: STEM: "+stem)
-            file = setUp(event, "33-34Savitri")
+            file = setUp("33-34Savitri")
             //console.log("getInput: DB: "+db)
             sectionalPrint(file, stem, "33-34Savitri")
         }
@@ -87,8 +149,8 @@ function Parser(userInput) {
     if ((userInput.split(" ").length) < 2) { return userInput; }
 } 
 
-function newParser(userInput, event) { 
-    list_of_dbs = ["keys", "arex", "QQ", "gloss", "san", "iye", "aqal", "occ","auth", "cats", "syn", "jbkeys", "todo", "most", "33-34Savitri"]
+function newParser(userInput, event) {  // listlist
+    list_of_dbs = ["keys", "arex", "argex", "good", "QQ", "gloss", "san", "iye", "aqal", "occ","auth", "cats", "syn", "jbkeys", "todo", "most", "33-34Savitri", "super", "fulldatabase", "cleaned", "major", "wikiauthors"]
     list_of_special_commands = ["process auth"]
 
     userInput = userInput.toLowerCase();
@@ -116,6 +178,16 @@ function newParser(userInput, event) {
     return userInput
 }
 
+function new_removeChildren(id) { // like output, or demo
+ for (x = 0; x < document.getElementById(id).childNodes.length; x++) { 
+        document.getElementById(id).removeChild(document.getElementById(id).childNodes[x])
+    }
+    if ( document.getElementById(id).childNodes.length > 0) { 
+        new_removeChildren()
+    }
+}
+
+
 
 function removeChildren() {
     for (x = 0; x < document.getElementById("output").childNodes.length; x++) { 
@@ -126,18 +198,12 @@ function removeChildren() {
     }
 }
 
-
-function myOverFunction(text) {
-    //document.getElementById("demo3").innerHTML = y+=1;
-    console.log("myOverFunction: "+text)
-}
-
 function libraryProcessor() {
 
 }
 
 function prePrint(event, db, userInput, oldInputForm) {
-    req = setUp(event, db)
+    req = setUp(db)
     consoleInfo("userInput: "+userInput)
     consoleInfo("typeof(oldInputForm): "+typeof(oldInputForm))
     //consoleInfo(db)
@@ -147,6 +213,17 @@ function prePrint(event, db, userInput, oldInputForm) {
         console.log("prePrint: IF"+userInput)
         quickPrint(req, userInput, db)
     }
+
+    //acacac
+    //else if ( userInput.match("^ac ") { authCount(event, db, 7) }
+    else if ( userInput == "ac arex" ) { authCount(event, "arex", 15) }
+    else if ( userInput == "ac keys" ) { authCount(event, "keys", 3) }
+    else if ( userInput == "ac good" ) { authCount(event, "good", 7) }
+    else if ( userInput == "ac argex" ) { authCount(event, "argex", 7) }
+    else if ( userInput == "ac super" ) { authCount(event, "super", 15) }
+    else if ( userInput == "ac cleaned" ) { authCount(event, "cleaned", 3) }
+    else if ( userInput == "ac major" ) { authCount(event, "major", 5) }
+    else if ( userInput == "ac fulldatabase" ) { authCount(event, "fulldatabase", 15) }
 
     else if ( userInput == "Savitri" ) {
         //document.write("  _/\           /\_ ")
@@ -162,6 +239,12 @@ function prePrint(event, db, userInput, oldInputForm) {
 
     else if (userInput == "lib") {
         window.location.replace("file:///home/oem/Documents/Code/KEYS/the_lib.html");
+    }
+
+    // ADDDDDDDDDD TO DATABASE!!!!
+    // dbdbdbdbdb
+    else if (db == "good" || db == "argex" || db == "super" || db == "fulldatabase" || db == "cleaned" || db == "major" ) {
+        quickPrint(req, userInput, db)
     }
 
     else if (db == "todo" || db == "cats" || db == "keys" ) {
@@ -236,6 +319,7 @@ function colorPrint(file, input, db) {
 
 
 function regexPrint(file, input, db) { 
+    var sav_index = 0
     var count = 0
     var query = new RegExp(".*"+input+".*", "gim");
 	values = file.responseText.match(query);
@@ -248,19 +332,23 @@ function regexPrint(file, input, db) {
             document.getElementById("output").appendChild(document.createTextNode(values[x]));
         }
 
-        else if (db != "33-34Savitri") {
+        else if (values[x].includes(", Savitri")) { // QUERY FOR SAVITRI?
             console.log("regexPrint: db != 33-34Savitri")
-            document.getElementById("output").innerHTML += values[x]+" "+"[!"+x+"]";
+            console.log(sav_index.length+"sav_index||savitri_list"+savitri_list.length)
+            document.getElementById("output").innerHTML += values[x]+" "+"[!"+sav_index+"]";
             savitri_list.push(values[x]);
+            sav_index ++
             //console.log("savitri_list[\"5\"]: "+savitri_list[5])
         }
 
-        if (db == "33-34Savitri") {
+        else if (db == "33-34Savitri") { //
             console.log("regexPrint: db == 33-34Savitri")
             document.getElementById("output").innerHTML += values[x];
 //            document.getElementById("output").innerHTML += "<div onmouseover=\"myOverFunction("+values[x]+")\">"
   //          document.getElementById("output").innerHTML += "<p id=\"demo3\">:"+x+"</p></div>"
         }
+
+        else { document.getElementById("output").innerHTML += values[x]; }
 
         if (db != "todo" && values[x] != "") { 
             document.getElementById("output").appendChild(document.createElement("br"));
@@ -274,23 +362,32 @@ function regexPrint(file, input, db) {
     document.getElementById("output").appendChild(document.createTextNode(count));
 }
 
+function quickListAFile(file, input) { // (file, ".", db)
+
+    var query = new RegExp(".*"+input+".*", "gim");
+    values = file.responseText.match(query);
+
+    for (var x = 0; x < values.length; x++) {
+        filebucket.push(values[x]);
+    }
+}
+
 
 function quickPrint(file, input, db) {
     var count = 0
     var query = new RegExp(".*"+input+".*", "gim");
 	values = file.responseText.match(query);
 
-    for (var x = 0; x < values.length; x++) {
-        
+    for (var x = 0; x < values.length; x++) {       
         document.getElementById("output").appendChild(document.createTextNode(values[x]));
         if (db != "todo" && values[x] != "") { 
             document.getElementById("output").appendChild(document.createElement("br"));
+            //document.getElementById("output").appendChild(document.createElement("br"));
+        }
+        //lblblblblb
+        if (db == "todo" || db == "super" || db == "fulldatabase" || db == "major" || db == "cleaned" && values[x] != "") {
             document.getElementById("output").appendChild(document.createElement("br"));
         }
-        if (db == "todo" && values[x] != "") {
-            document.getElementById("output").appendChild(document.createElement("br"));
-        }
-
         count++;
     }
     document.getElementById("output").appendChild(document.createTextNode(count));
@@ -340,6 +437,80 @@ function sectionalPrint(file, input, db) {
     }
 }
 
+
+function superCount(arr) {
+    var a = [], b = [], prev;
+
+    arr.sort();
+    for ( var i = 0; i < arr.length; i++ ) {
+        if ( arr[i] !== prev ) {
+            a.push(arr[i]);
+            b.push(1);
+        } else {
+            b[b.length-1]++;
+        }
+        prev = arr[i];
+    }
+
+    return [a, b];
+}
+
+
+function test_out(auth, x) {
+    var div = document.createElement("div"+x);
+
+    //div.style.width = "100px";
+    //div.style.height = "100px";
+    //div.style.background = "red";
+    //div.style.color = "white";
+    div.innerHTML = '<div id="demo" onmouseover="mouseOn('+x+')" onmouseout="mouseOff('+x+')"><p><b>'+auth+'</b> (<span id="demo'+x+'"></span>)</p></div>'
+
+    document.getElementById("output").appendChild(div);
+}
+
+
+function authCount(event, db, moreCountsThen) {
+    file = setUp(db)
+    quickListAFile(file, ".")
+    r = 0
+    for (x = r; x < filebucket.length; x++) { 
+        i = filebucket[x].indexOf("~")
+        comma = filebucket[x].indexOf(",")
+        relative_comma = filebucket[x].slice(i, filebucket[x].length).indexOf(",")
+        tag = filebucket[x].indexOf("#")
+
+        if ( comma == "-1" ) { comma = filebucket[x].length }
+        if ( relative_comma == "-1" ) { relative_comma = filebucket[x].length }
+        if ( tag == "-1" ) { tag = filebucket[x].length }
+ 
+        the_cut = filebucket[x].slice(i+2, Math.min(filebucket[x].length-1, relative_comma+i, tag))
+        the_cut = the_cut.trim()
+        authbucket.push(the_cut);
+    } 
+
+    superArray = superCount(authbucket)
+    bySize = []
+
+    for (x = 0; x < authbucket.length; x++) {
+        if (superArray[1][x] > moreCountsThen) {
+            //bySize.push(superArray[1][x]+" :: "+superArray[0][x])
+
+            //document.getElementById("output").appendChild(document.createTextNode(superArray[0][x]+" :: "+superArray[1][x]));
+            //document.getElementById("output").appendChild(document.createElement("br"));
+            //document.getElementById("output").appendChild(document.createTextNode());
+            test_out(superArray[0][x], x)
+            //console.log("authbucket.length "+authbucket.length+authbucket[x])
+
+        }
+    }
+    //window.stop();
+}
+//document.getElementById("output").appendChild(document.createTextNode(values[x]));
+
+//<div id="demo" onmouseover="MouseOn()" onmouseout="MouseOff()"><p><b>Sri Aurobindo</b> (<span id="demo2"></span>)</p></div>
+
+
+// EVENT LISTENER
 window.addEventListener('keyup', function (event) { // disables some keys
     if (event.keyCode === 9) { //left tab
         event.preventDefault();
@@ -364,9 +535,15 @@ window.addEventListener('keyup', function (event) { // disables some keys
     }
 });
 
+    wikit_list = []
+    filebucket = []
+    authbucket = []
+    //document.getElementById("myinput").addEventListener("keyup", authCount); 
+
     savitri_list = []
     db = ""
-    //document.getElementById("myinput").addEventListener("keyup", getInput); 
-  addEventListener("keyup", getInput);
+    addEventListener("keyup", getInput);
 
-</script>
+
+
+//document.getElementById("myinput").addEventListener("keyup", getInput); 
