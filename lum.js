35f65b96b2836b7462db5d311c91760d9349996c
integralyogin <integralyogin@gmail.com>
2019-01-21 09:17:08 -0500

    oh boy let me tell you..
    this hot mess can now sometimes print out text surrounding the quote from the book.
    aka grabs quote in book, and prints surrounding text. mhmm.
    hot mess.

--- lum.js
+++ lum.js
@@ -1,7 +1,11 @@
-    filebucket = []         //file gets loaded into this array
+    list_of_dbs = []
+		filebucket = []         //file gets loaded into this array
     authbucket = []
     savitri_list = []
+		booklist = []
     db = ""
+
+		getBookList()
 
 window.addEventListener("keyup", getInput); // document.getElementById("myinput").addEventListener("keyup", authCount); 
 window.addEventListener('keyup', function (event) { // disables some keys
@@ -13,11 +17,10 @@ window.addEventListener('keyup', function (event) { // disables some keys
         if (active == "home") { console.log("addEventListener: home: "+active); scroll(0,0) }
     }
 });
-   
 
 function getInput(event) {
     userInput = document.getElementById("myinput").value;
-    if (event.keyCode == 13) { // ENTER KEYPRESS
+    if (event.keyCode == 13) { 																			// ENTER KEYPRESS
         //InputLog()
         //getIp()
         db = ""
@@ -37,6 +40,7 @@ function getInput(event) {
 function newParser(userInput, event) {          // listlistlistlistdbdbdb
     list_of_dbs = ["keys", "major", "fulldatabase", "33-34Savitri", "gloss", "auth", "wikiauthors", "san", "iye", "aqal", "occ", "cats", "syn", "jbkeys", "todo", "most", "inv"]
     userInput = userInput.toLowerCase();
+		if (userInput == "lib") { window.location.replace("http://157.230.133.200/the_library.php"); }
     if ((userInput.split(" ").length) > 1) { userInput = userInput.split(" "); }
     if (db == "") { db = "keys" }
     for (x = 0; x < list_of_dbs.length; x++) {  // if userInput[0] is database
@@ -63,6 +67,8 @@ function prePrint(event, db, userInput, oldInputForm) {
     req = setUp(db)
     userInput = document.getElementById("myinput").value;
     console.log("userInput/oldInputForm: "+userInput+":"+oldInputForm)
+		console.log("event: "+event)
+		console.log(document)
     if (userInput[0] == "!" && savitri_list.length > 0) { // SAVITRI SECTIONAL PRINT
         new_removeChildren("output")
         indexNum = userInput.slice(1)
@@ -72,19 +78,31 @@ function prePrint(event, db, userInput, oldInputForm) {
         file = setUp("33-34Savitri")
         sectionalPrint(file, stem, "33-34Savitri")
     }
-    else if (userInput.length < 2 && db == "keys") { quickPrint(req, userInput, db) }
+    else if (userInput.length < 2 && db == "keys") { quickPrint(req, userInput, db) } //still too slow, maybe partial releasing?
     /// acacacac 
-    else if ( userInput.toLowerCase() == "ac keys" ) { authCount(event, "keys", 3) }
+    else if ( userInput.toLowerCase() == "ac keys" ) { authCount(event, "keys", 0) }
     else if ( userInput.toLowerCase() == "ac major" ) { authCount(event, "major", 5) }
     else if ( userInput.toLowerCase() == "ac fulldatabase" ) { authCount(event, "fulldatabase", 2) }
     // links
     else if ( userInput == "lib") { window.location.replace("file:///home/oem/Documents/Code/KEYS/the_lib.html"); }
     /// dbdbdbdbdb
-    else if ( db == "fulldatabase" || db == "major" || db == "wikiauthors") { quickPrint( req, oldInputForm, db ) } // QUICKPRINT FOR BIG DATABASES
-    else { regexPrint( req, oldInputForm, db ) }
+    else if ( db == "fulldatabase" || db == "major") { quickPrint( req, oldInputForm, db ) } // QUICKPRINT FOR BIG DATABASES
+    else { 
+				regexPrint( req, oldInputForm, db );
+		}
 
 }
 
+function clickRegexPrint(db, clickInput) {
+	req = setUp(db)
+	regexPrint(req, clickInput, db)
+}
+
+function clickToLibrary(clickInput) {
+
+	// > python libsearcher.py clickInput
+
+}
 
 function setUp(filename) {
     var req = new XMLHttpRequest();
@@ -97,14 +115,17 @@ function setUp(filename) {
 
 function check_address(filename, req) { 
     address = String(window.location.pathname)
-    if (address == "/home/oem/Documents/Code/KEYS/lum.html") { console.log("LOCAL");
-        return filename
-    } else { 
-        console.log("ONLINE");    
-        newfilename = "https://raw.githubusercontent.com/integralyogin/data-for-lum/master/"+filename
+		console.log("check address: filename: "+filename)
+    if (address == "/home/oem/Documents/Code/KEYS/lum.html") { console.log("LOCAL"); return filename; } 
+		else if (filename == "booklist") { newfilename = "textfiles/booklist"; return newfilename; }
+		else if (filename.includes(".txt")) { newfilename = "textfiles/"+filename.slice(1); return newfilename; }
+		else { console.log("check address: else, use local"); newfilename = "dbs/"+filename; console.log("newfilename: "+newfilename); return newfilename; }
+		//else { 
+     //   console.log("ONLINE");    
+      //  newfilename = "https://raw.githubusercontent.com/integralyogin/data-for-lum/master/"+filename
         //if (filename.includes('fulldatabase')) { newfilename = "dbs/fulldatabase" } 
-				return newfilename
-    }
+		//		return newfilename
+   // }
 }
 
 // ---------------------------------------------------------------------//
@@ -115,25 +136,36 @@ function check_address(filename, req) {
 function regexPrint(file, input, db) { 
     var sav_index = 0
     var count = 0
+		//console.log("input: "+input)
     var query = new RegExp(".*"+input+".*", "gim");
-	values = file.responseText.match(query);
+		values = file.responseText.match(query); // values = search results?
     savitri_list = []
+		//console.log("values, typeof values, length: "+values+":"+(typeof values))
+		if (values == null ) { zeroHandler(input, db); }
     for (var x = 0; x < values.length; x++) {
-        if (values[x].includes(", Savitri")) { // QUERY FOR SAVITRI?
-            //console.log("regexPrint: db != 33-34Savitri")
-            //console.log(sav_index.length+"sav_index||savitri_list"+savitri_list.length)
-            document.getElementById("output").innerHTML += values[x]+" "+"[!"+sav_index+"]";
-            savitri_list.push(values[x]);
-            sav_index ++
-        } else if (db == "33-34Savitri") { document.getElementById("output").innerHTML += values[x];
-        } else { document.getElementById("output").innerHTML += values[x]; }
+				var rvalue = values[x].replace(/\s/g, '&nbsp;');
+				var rvalue = strip(rvalue)
+				var div = document.createElement("div"+x);
+				div.innerHTML += '<div id="demo"><p>'+values[x]+'<a href=javascript:bookFinder("'+rvalue+'",'+x+')>...</a><span id="demo'+x+'"></span></p></div><br><br>' //bookfinder
+				document.getElementById("output").appendChild(div);
+						
+	//					document.getElementById("output").innerHTML += "<a href=javascript:bookFinder("+values[x]+")...</a><br><br>"
+
+			 	// CONTENT ADD
+
         if (db != "todo" && values[x] != "") { 
             document.getElementById("output").appendChild(document.createElement("br"));
             document.getElementById("output").appendChild(document.createElement("br"));
         }
         if (db == "todo" && values[x] != "") { document.getElementById("output").appendChild(document.createElement("br")); }
         count++;
-    } document.getElementById("output").appendChild(document.createTextNode(count));
+    }
+		document.getElementById("output").appendChild(document.createTextNode(count)); 
+		document.getElementById("output").innerHTML += "<br><a href=javascript:new_removeChildren('output')>Clear Output</a><br>"
+		var input = input.replace(/\s/g, '&nbsp;');
+		document.getElementById("output").innerHTML += "<a href=javascript:clickRegexPrint('major','"+input+"')>Extend Search into Major DB (can take a while)*</a><br><br>"
+
+
 } // QUICKPRINT
 function quickPrint(file, input, db) {              
     var count = 0
@@ -153,7 +185,9 @@ function sectionalPrint(file, input, db) {
     var query = new RegExp(".*"+input+".*", "gim");
     var setUp = new RegExp(".*"+"."+".*", "gim");
 	newvalues = file.responseText.match(setUp);
-    for (x = 0; x < newvalues.length; x++) {            // LOCATE IN BOOK
+ 	// find book file address.
+
+		for (x = 0; x < newvalues.length; x++) {            // LOCATE IN BOOK
         if (newvalues[x].includes($.trim(input))) { console.log("fixed mind: "+newvalues[x]+" + "+x); }
         if (newvalues[x].includes($.trim(input))) {
             location = x
@@ -166,46 +200,62 @@ function sectionalPrint(file, input, db) {
     }
 }
 
+function quickListAFile(file, input) { // (file, ".", db)
+    var query = new RegExp(".*"+input+".*", "gim");
+    values = file.responseText.match(query);
+    for (var x = 0; x < values.length; x++) { filebucket.push(values[x]); }
+}
+
 function authCount(event, db, moreCountsThen) {         // AC
     console.log(document)
-    file = setUp(db)
-    quickListAFile(file, ".")
+    file = setUp(db) //keys, major, full
+    quickListAFile(file, ".") // fill file bucket
     r = 0
 
-    for (x = r; x < filebucket.length; x++) {           // FILL FILEBUCKET
+    for (x = r; x < filebucket.length; x++) { // go through file bucket
+				//get indexes
         i = filebucket[x].indexOf("~")
         comma = filebucket[x].indexOf(",")
         relative_comma = filebucket[x].slice(i, filebucket[x].length).indexOf(",")
         tag = filebucket[x].indexOf("#")
-        if ( comma == "-1" ) { comma = filebucket[x].length }
+
+        if ( comma == "-1" ) { comma = filebucket[x].length } //if doesnt exist, you come in last place
         if ( relative_comma == "-1" ) { relative_comma = filebucket[x].length }
         if ( tag == "-1" ) { tag = filebucket[x].length }
-        the_cut = filebucket[x].slice(i+2, Math.min(filebucket[x].length-1, relative_comma+i, tag))
-        the_cut = the_cut.trim()
-        authbucket.push(the_cut);
+
+        auth_cut = filebucket[x].slice(i+2, Math.min(filebucket[x].length, relative_comma+i, tag))
+        auth_cut = auth_cut.trim()
+        authbucket.push(auth_cut); // fill auth bucket
+
+				// show head of both buckets:
         if (x < 3) { console.log("(n authCount) x:authbucket[x]:filebucket[x] >>> "+x+":"+authbucket[x]+":"+filebucket[x]); }    //>>> 2:Socrates:"Be as you wish to seem. ~ Socrates"
+
     } superArray = superCount(authbucket)
+
     if (typeof counterA === 'undefined') { counterA = 0 }
-    for (x = 0; x < authbucket.length; x++) {           // MAKE DIV OF SUPERARRAY
-        if (superArray[1][x] > moreCountsThen) { 
-            makeDiv(superArray[0][x]+"</b>: ("+superArray[1][x]+" results in "+db+")", x) 
+    for (x = 0; x < authbucket.length; x++) {  									// go through superarray (auth)
+        if (superArray[1][x] > moreCountsThen) {  															//if its enough:
+            makeDiv(superArray[0][x]+"</b>: ("+superArray[1][x]+" results in "+db+")", x)  // add it to the div
+								console.log("superarray: 0x, 1x: "+superArray[0][x]+":"+superArray[1][x])
             counterA += 1
         } 
     } document.getElementById("output").appendChild(document.createTextNode(counterA+" authors listed with at least x results"));  //window.stop();
 } 
+
 function makeDiv(auth, x) {
     var div = document.createElement("div"+x);
     div.innerHTML = '<div id="demo" onmouseup="mouseClick('+x+')" onmouseout="mouseOff('+x+')"><p><b>'+auth+' (+<span id="demo'+x+'"></span>)</p></div>'
     document.getElementById("output").appendChild(div);
 }
+
 function mouseOff(x) { new_removeChildren("demo"+x) }
-function mouseClick(x) {
+function mouseClick(x) { // x = authbucketLineNumber + demoID?
     wikit = setUp("wikiauthors")
     var node = document.getElementById('demo')
     textContent = node.childNodes[0].innerHTML
     text_length = textContent.length
     var query = new RegExp("^"+""+".*", "img");
-    entry = (wikit.responseText.match(query, "img"));
+    wikifile = (wikit.responseText.match(query, "img"));
     authName = textContent.slice(1, text_length-4)
     auth = ""
     for (var i = 0; i < document.all.namedItem("demo").length; i++) {  //860 entries
@@ -215,32 +265,27 @@ function mouseClick(x) {
             } 
         }
     }  
-    for (i = 0; i < entry.length; i++) { if (entry[i].indexOf(auth) == 0) { r = i }}  // GETS ENTRIES THAT START WITH AUTH NAME (can be removed)
+    for (i = 0; i < wikifile.length; i++) { if (wikifile[i].indexOf(auth) == 0) { r = i }}  // GETS ENTRIES THAT START WITH AUTH NAME (can be removed)
     if (r == "") {
         indexOfName = []
         ENTRIES = []
-        for (i = 0; i < entry.length; i++) {
-            if (entry[i].includes(auth)) {             
+        for (i = 0; i < wikifile.length; i++) {
+            if (wikifile[i].includes(auth)) {             
                 ENTRIES.push(i)
-                indexOfName.push(entry[i].indexOf(auth))
+                indexOfName.push(wikifile[i].indexOf(auth))
             }
         }
         idx=indexOfName.indexOf(Math.min.apply(null,indexOfName))
         r = ENTRIES[idx]
     } //soMuchInfo()
 //   document.getElementById("demo"+x).appendChild(document.createTextNode(entry[r]));
-   document.getElementById("demo"+x).innerHTML += "<p>"+entry[r]
+   document.getElementById("demo"+x).innerHTML += "<p>"+wikifile[r]
+	console.log("wikifile[r]: "+wikifile[r])
    r = ""
    ENTRIES = []
    indexOfName = []
 }
 
-
-function quickListAFile(file, input) { // (file, ".", db)
-    var query = new RegExp(".*"+input+".*", "gim");
-    values = file.responseText.match(query);
-    for (var x = 0; x < values.length; x++) { filebucket.push(values[x]); }
-}
 
 
 function soMuchInfo(x) { console.log(document.all) }
@@ -251,8 +296,9 @@ function new_removeChildren(id) {
         document.getElementById(id).removeChild(document.getElementById(id).childNodes[x])
     }
     if ( document.getElementById(id).childNodes.length > 0) { new_removeChildren(id) }
+		document.getElementById("myinput").focus(); //scroll(0,0); (scroll here broke ac keys scrolling)
 }
-
+   
 
 function InputLog() {
 
@@ -277,6 +323,132 @@ function superCount(arr) {
     prev = arr[i];
     } return [a, b]; 
 }
+
+function zeroHandler(input, db) {
+	for (var i = 0; i < list_of_dbs.length; i++ ) {
+		console.log("input, db: "+input+":"+db)
+		console.log("typeof input/db: "+(typeof input)+":"+(typeof db))
+		if (input == list_of_dbs[i]) {
+				var input = input.replace(/\s/g, '&nbsp;');
+			document.getElementById("output").innerHTML += "Input '"+input +"' is a database named '"+list_of_dbs[i]+"'<br>"
+			document.getElementById("output").innerHTML += "If you wished to search for '"+input+"' you can try 'keys "+input+"'<br><br>"
+			document.getElementById("output").innerHTML += "<a href=javascript:clickRegexPrint('major','"+input+"')>Search for '"+input+"' in 'major'</a><br><br>"
+					
+		} 
+	} 
+	var input = input.replace(/\s/g, '&nbsp;');
+	document.getElementById("output").innerHTML += "Sorry there were no results for '"+input+"' in database '"+db+"'.<br>"
+	document.getElementById("output").innerHTML += "<a href=javascript:clickRegexPrint('major','"+input+"')>Search for '"+input+"' in 'major'</a><br><br>"
+}
+
+function getBookList() {
+	req = setUp("booklist")
+  var query = new RegExp(".*/.*", "gim");
+	values = req.responseText.match(query); // values = search results?
+	//console.log("getBookList: values.length: "+values.length)
+	booklist.push(values)
+	//console.log("getBookList: booklist[0].length: "+booklist[0].length)
+}
+
+function bookFinder(line, xloc) {
+//		console.log("bookfinder: line: "+line)
+//	console.log("left side::: "+line.split("~")[0])
+	if (line.includes("~")) { 
+			leftside = line.split("~")[0];
+			rightside = line.split("~")[1];	
+//		console.log("rightside: "+rightside) 
+			if (rightside.includes(",")) { 
+				var count = (rightside.match(/,/g) || []).length;
+		//	console.log(", count: "+count)
+				for (var x = 0; x <= count; x++) {
+					if (x == 0) { auth = rightside.split(",")[0];	}
+					if (x == 1) { book = rightside.split(",")[1]; console.log("book"); }
+					if (x == 1) { console.log("auth/book: "+auth+":"+book); }
+				}
+				if (typeof auth !== 'undefined' && typeof book !== 'undefined') {
+					for (var x = 0; x < booklist[0].length; x++) {
+						var spaceless = book.replace(/\s/g, '');
+						var underscoredtitle = book.replace(/\s/g, '_');
+						//console.log("spaceless: "+spaceless)
+						if (booklist[0][x].includes("INTEG")) {
+							console.log("booklist[0][x]:spaceless:underscoredtitle: "+booklist[0][x]+":"+spaceless+":"+underscoredtitle)
+						}
+						if (booklist[0][x].includes(spaceless) || booklist[0][x].includes(underscoredtitle)) {
+							console.log("found book!: "+booklist[0][x])
+									inBook(booklist[0][x], leftside, xloc) // inBook
+						}
+						if (booklist[0][x].includes(auth.split(" ")[auth.split(" ").length - 1].toUpperCase())) {
+							console.log("author bingo! "+booklist[0][x])
+						}
+					}
+				}
+				if (typeof auth !== 'undefined') { 
+					//check if in booklist
+					for (var x = 0; x < booklist[0].length; x++) {
+						if (booklist[0][x].includes(auth.split(" ")[auth.split(" ").length - 1].toUpperCase())) {
+					//		console.log("author bingo! "+booklist[0][x])
+						}
+						else {
+						//	console.log(auth.split(" ")[auth.split(" ").length - 1].toUpperCase())
+						}
+					}
+				}
+				if (typeof book !== 'undefined')  { console.log(book); }
+					for (var x = 0; x < booklist[0].length; x++) {
+				}
+			}
+	//parse right side for auth, and book.
+	// verify auth from auth list
+	// verify book from book list
+	}
+
+}
+
+function inBook(booklocation, leftside, xloc) {
+	console.log("inBook: "+booklocation+":"+leftside)
+	req = setUp(booklocation)
+	console.log("inBook: leftside: '"+leftside.slice(4,leftside.length - 1)+"'")
+  //var query = new RegExp(".*"+leftside.slice(4,leftside.length - 1)+".*", "gim");
+	var query = new RegExp(".*", "gim");
+	values = req.responseText.match(query); // values = search results?
+	console.log("values.length: "+values.length)
+	cleanleft = leftside.replace(/\s/g, ' ');
+	console.log("leftside: "+leftside)
+	var found = 0;
+	for (var s = 0; s < cleanleft.length-1; s++) {
+		if (s == 0) { cleanslice = cleanleft.slice(s, cleanleft.length-s) }
+		if (s == 1) { cleanslice = cleanleft.slice(1, 20) } 
+		console.log("cleanslice--s: "+cleanslice+"--"+s)
+			for (var x = 0; x <= values.length; x++) {
+				if (typeof cleanslice === "undefined") { console.log("break, its undefined") }
+				//console.log("typeof cleanslice: "+typeof cleanslice+":"+cleanslice+":"+x)
+				if (values[x].includes(cleanslice)) {
+					for (var z = -14; z < 14; z++) {
+						if (values[x+z] != "") {
+							if (z != 0) { document.getElementById("demo"+xloc).innerHTML += "<p>"+values[x+z] }
+							if (z == 0) { document.getElementById("demo"+xloc).innerHTML += "<p><b>"+values[x+z]+"</b>" }
+						}
+					}
+					found = 1;
+					break;
+				}
+				if (x == values.length) { 
+						console.log(x+" @ EOF") 
+						console.log("EOF :cleanslice: "+cleanslice)
+				}
+
+			}
+		if (found == 1) { break; }
+	}
+}
+
+
+function strip(html) {
+	var tmp = document.createElement("DIV");
+	tmp.innerHTML = html;
+	return tmp.textContent || tmp.innerText || "";
+}
+
 
 
 document.getElementById("endofhtml").innerHTML = "lastModified: "+document.lastModified
